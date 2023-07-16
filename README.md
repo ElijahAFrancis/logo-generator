a495a2ee35bdb04e58087f6530f59f86938936c9
[Elijah <[efrancis64@gmail.com]>
2023-07-16 13:22:22 -0400

    added tests, changed logo generation process.

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# logo-generator
+# README Generator
+  ![License](https://img.shields.io/badge/license-MIT-yellowgreen.svg)
+  ## Table of Contents
+  [Description](#description)
+  [Installation](#installation)
+  [Usage](#usage)
+  [Questions](questions)
+  [License](license)
+
+  ## Description
+  Generates an SVG logo
+  https://drive.google.com/file/d/1Hwxk2aPP0QpgTfjchacpGHIVMwYRdraC/view
+
+  ## Installation
+  Clone github repository
+
+  ## Usage
+  Type 'npm i' into your console
+  Type 'node index.js' into your console and enter responses to each prompt
+
+  ## Questions
+  https://github.com/ElijahAFrancis
+  efrancis64@gmail.com
+
+  ## License
+    Licensed under the MIT license
+    https://www.mit.edu/~amini/LICENSE.md
+  
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
-const generateLogo = require('./utils/generateLogo');
+const {triangle, circle, square} = require('./lib/shapes.js');
+const { writeFile } = require('fs/promises');
 
 const questions = [
     {
@@ -12,30 +13,41 @@ const questions = [
     {
         type: 'input',
         name: 'textColor',
-        message: 'What color would you like your logo text to be?'
+        message: 'What color would you like your logo text to be? Can be keyword or hexadecimal number.'
     },
     {
         type: 'list',
         name: 'shape',
         message: 'What shape would you like your logo to be?',
-        choices: ['triange', 'circle', 'square']
+        choices: ['triangle', 'circle', 'square']
     },
     {
         type: 'input',
         name: 'color',
-        message: 'What color would you like your logo to be?'
+        message: 'What color would you like your logo to be? Can be keyword or hexadecimal number.'
     }
 ];
-
-function writeToFile(fileName, data) {
-    fs.writeFileSync(path.join(process.cwd(), fileName), data)
-}
 
 function init() {
     inquirer.prompt(questions)
     .then((answer) => {
         console.log(answer);
-        writeToFile('./logo.svg', generateLogo(answer))
+        let shape
+        if(answer.shape == 'triangle') {
+            shape = new triangle(answer.color);
+        } else if(answer.shape == 'circle') {
+            shape = new circle(answer.color);
+        } else if(answer.shape == 'square') {
+            shape = new square(answer.color);
+        }
+        console.log(shape)
+        const svg = `
+                        <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+                            ${shape.render()}
+                            <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answer.textColor}">${answer.text}</text>
+                        </svg>
+                        `
+        fs.writeFileSync('./logo.svg', svg)
     });
 }
 
